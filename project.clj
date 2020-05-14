2e31b67477cbca156e5e4b974cb829088babddec
Aisha Blfgeh <aisha.blfgeh@gmail.com>
2020-05-14 05:39:41 +0100

    update the description

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject amino "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Amino acids ontology developed using an Excel
+  spreadsheet as a source of information"
+  :url "http://example.com/aao"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
